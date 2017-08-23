6543d96634fc157106f71d56e8bfb11db936c016
Sujee Maniyam (sujee-mac2) <sujee@sujee.net>
2017-08-23 11:49:47 -0700

    prosper loan data

--- presidential_election_contribs/download-data.sh
+++ presidential_election_contribs/download-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#wget https://s3.amazonaws.com/elephantscale-public/data/presidential_election_contribs/2016.zip
+wget https://s3.amazonaws.com/elephantscale-public/data/presidential_election_contribs/2016.zip
 
 wget https://s3.amazonaws.com/elephantscale-public/data/presidential_election_contribs/2012.zip
