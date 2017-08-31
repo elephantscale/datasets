3c9bfb3dba6432e427222c68a98b56e59547da64
Tim Fox <tim@elephantscale.com>
2017-08-31 13:52:44 -0400

    checked in small version of movielens dataset

--- movielens/download.sh
+++ movielens/download.sh
@@ -4,8 +4,8 @@ hash wget 2>/dev/null || { echo >&2 "Wget required.  Aborting."; exit 1; }
 hash unzip 2>/dev/null || { echo >&2 "unzip required.  Aborting."; exit 1; }
 
 wget 'http://files.grouplens.org/datasets/movielens/ml-latest.zip'
-wget 'http://files.grouplens.org/datasets/movielens/ml-latest-small.zip'
+#wget 'http://files.grouplens.org/datasets/movielens/ml-latest-small.zip'
 
 unzip -o "ml-latest.zip"
-unzip -o "ml-latest-small.zip"
+#unzip -o "ml-latest-small.zip"
 mv ml-latest full
