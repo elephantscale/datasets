24094e903fd4a255b541b5def563ec48baf3c441
Sujee Maniyam @ sujee-mac2 <sujee@sujee.net>
2018-01-14 00:22:16 -0800

    uber download script fix

--- uber-nyc/download-data.sh
+++ uber-nyc/download-data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget https://s3.amazonaws.com/elephantscale-public/data/uber-nyc/*
+wget https://s3.amazonaws.com/elephantscale-public/data/uber-nyc/uber-nyc.zip
+
+unzip uber-nyc.zip
