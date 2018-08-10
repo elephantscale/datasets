096bfa7c54c4cf418fe1899574eb38202f63a99c
Sujee Maniyam <sujee@sujee.net>
2018-08-10 09:19:35 -0700

    ML : s3 upload script [1]

--- upload-to-s3.sh
+++ upload-to-s3.sh
@@ -9,11 +9,15 @@
 ##      aws_access_key_id =  XXXXX
 ##      aws_secret_access_key = YYYYY
 
+
+echo "## uploading to S3"
+aws s3 sync --exclude '.git/*' --exclude 'nltk_data/*'  --exclude 'activity-data/*' --exclude '**/.DS_Store'  . s3://elephantscale-public/data/ --acl public-read
+
+
 zip_file_name="datasets.zip"
 rm -f *.zip
 echo "## creating $$zip_file_name...."
 zip -x '*.DS_Store*'  -x "*.log" -x '*.git*'  -x '*zip*'  -x '*metastore_db*' -x '*out' -x '*.ipynb_checkpoints*' -x '*not-using*' -x 'nltk_data/*' -x 'activity-data/*' -x 'presidential_election_contribs/2016.zip' -x 'presidential_election_contribs/2016/2016-full.csv.gz' -r "$zip_file_name" .
-
 echo "## done."
 
 echo "## uploading to S3"
