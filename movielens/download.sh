23475843a3a380f9fc11ca59ca9f32329b0bd732
Sujee Maniyam <sujee@sujee.net>
2018-10-16 23:57:07 -0700

    ML : movielense download script fix [1]

--- movielens/download.sh
+++ movielens/download.sh
@@ -3,9 +3,19 @@
 hash wget 2>/dev/null || { echo >&2 "Wget required.  Aborting."; exit 1; }
 hash unzip 2>/dev/null || { echo >&2 "unzip required.  Aborting."; exit 1; }
 
-wget 'http://files.grouplens.org/datasets/movielens/ml-latest.zip'
-#wget 'http://files.grouplens.org/datasets/movielens/ml-latest-small.zip'
+if [ ! -f "ml-latest.zip" ] ; then
+    echo "downloading ml-latest.zip"
+    wget 'http://files.grouplens.org/datasets/movielens/ml-latest.zip'
+    #wget 'http://files.grouplens.org/datasets/movielens/ml-latest-small.zip'
+else
+        echo "ml-latest.zip exists"
+fi
 
-unzip -o "ml-latest.zip"
-#unzip -o "ml-latest-small.zip"
-mv ml-latest full
+if [ ! -d "ml-latest-full" ] ; then
+    echo "unzipping ml-latest.zip"
+    unzip -o "ml-latest.zip"
+    #unzip -o "ml-latest-small.zip"
+    mv ml-latest ml-latest-full
+else
+    echo "ml-latest-full directory exists"
+fi
