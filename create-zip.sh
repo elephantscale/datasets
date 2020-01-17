ad5bcd2a0830afe3e9c3bb0cb0e338ba60c7d1c8
Sujee Maniyam <sujee@elephantscale.com>
2020-01-16 22:47:05 -0800

    create zip

--- create-zip.sh
+++ create-zip.sh
@@ -5,6 +5,6 @@ zip_file_name="data.zip"
 rm -f *.zip
 echo "## creating $zip_file_name...."
 
-cd .. && rm -f data && ln -sf ./datasets ./data && zip -x '*.DS_Store*'  -x '*click-stream/json*' -x "*.log" -x '*.git*'  -x '*zip*' -x '*.data' -x '*metastore_db*' -x '*out' -x '*.ipynb_checkpoints*' -x '*not-using*' -x '*nltk_data/*' -x '*activity-data/*' -x '*presidential_election_contribs/2016.zip' -x '*presidential_election_contribs/2016/2016-full.csv.gz' -x '*regression/*' -x '*flights/raw/*.csv*' -r "$zip_file_name" data && mv $zip_file_name  datasets/ &&  cd -
+cd .. && rm -f data && ln -sf ./datasets ./data && zip -x '*.DS_Store*'  -x '*click-stream/json*' -x "*.log" -x '*.git*'  -x '*zip*' -x '*.data' -x '*metastore_db*' -x '*out' -x '*.ipynb_checkpoints*' -x '*not-using*' -x '*nltk_data/*' -x '*activity-data/*' -x '*presidential_election_contribs/2016.zip' -x '*presidential_election_contribs/2016/2016-full.csv.gz' -x '*regression/*' -x '*flight-data/raw/*.csv*' -r "$zip_file_name" data && mv $zip_file_name  datasets/ &&  cd -
 
 echo "## done."
