037adfda6fbc1a1ac8a27274de708734d8e745c8
Sujee Maniyam <sujee@elephantscale.com>
2020-01-16 22:29:42 -0800

    flights data raw

---
+++ flight-data/raw/download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201501.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201502.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201503.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201504.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201505.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201506.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201507.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201508.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201509.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201510.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201511.csv.gz'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201512.csv.gz'
