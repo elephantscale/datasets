8191c37f2d6c92ffa76b51f5dd96ad0ea57b9eba
Sujee Maniyam <sujee@elephantscale.com>
2020-01-19 12:27:38 -0600

    flights data download script

--- flight-data/raw/download.sh
+++ flight-data/raw/download.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201501.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201502.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201503.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201504.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201505.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201506.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201507.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201508.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201509.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201510.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201511.csv.gz'
-wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201512.csv.gz'
+mkdir -p 2015
+
+cd 2015
+
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201501.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201502.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201503.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201504.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201505.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201506.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201507.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201508.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201509.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201510.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201511.csv'
+wget 'https://elephantscale-public.s3.amazonaws.com/data/flight-data/raw/201512.csv'
