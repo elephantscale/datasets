3b470610f7788b263ed2b070bd0b2ec6ae5eae82
Sujee Maniyam <sujee@elephantscale.com>
2020-07-03 16:49:09 -0700

    modifying scripts

--- upload-to-s3.sh
+++ upload-to-s3.sh
@@ -11,13 +11,8 @@
 
 rm *.zip
 
-echo "## uploading to S3"
-aws s3 sync --exclude '.git/*' --exclude 'nltk_data/*'  --exclude 'activity-data/*' --exclude '**/.DS_Store' --exclude '*.out' --exclude 'data/text/twinkle/*.data'  --exclude 'click-sream/json'  . s3://elephantscale-public/data/ --acl public-read
-
-# zip up cats-dogs, so it is efficient to download from S3
-zip -r cats-dogs.zip cat-dog/
-
 ./create-zip.sh
 
 echo "## uploading to S3"
-aws s3 sync --exclude '.git/*' --exclude 'text/twinkle/*.data'  --exclude 'nltk_data/*'  --exclude 'activity-data/*'  . s3://elephantscale-public/data/ --acl public-read
+aws s3 sync --exclude '.git/*' --exclude 'nltk_data/*'  --exclude 'activity-data/*' --exclude '**/.DS_Store' --exclude '*.out' --exclude '**/_SUCCESS'  --exclude 'data/text/twinkle/*.data'  --exclude 'click-sream/json' --exclude 'uber-nyc/full/*' --exclude 'images/**/*' . s3://elephantscale-public/data/ --acl public-read
+
