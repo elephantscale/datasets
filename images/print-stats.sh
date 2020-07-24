4cfff034a40855eae61dd89e0b152319fe437e6d
Sujee Maniyam <sujee@elephantscale.com>
2020-07-24 10:36:38 -0700

    adding rock paper scissors image set details

--- images/print-stats.sh
+++ images/print-stats.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-find . -type d -exec bash -c 'echo "{} : files=" `find "{}" -type f | wc -l`  ", size=" `du -skh "{}" | cut -f 1`' \;
+
+dir="."
+if [ -n "$1" ] ; then
+    dir=$1
+fi
+
+find "$dir" -type d -exec bash -c 'echo "{} : files=" `find "{}" -type f | wc -l`  ", size=" `du -skh "{}" | cut -f 1`' \;
 
